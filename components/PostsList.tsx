236f2d4cad51c76d025cd14699b7b5dbd753a177
lucasbnunes <lucasbnunes23@gmail.com>
2023-04-22 20:11:12 -0300

    feat: add loading an empty states in the posts list

--- components/PostsList.tsx
+++ components/PostsList.tsx
@@ -1,13 +1,19 @@
 import { useGetPostsQuery } from "@/redux/slices/apiSlice";
-import { Box, Skeleton } from "@mantine/core";
+import { Box, Loader, Text } from "@mantine/core";
 import { Post } from "./Post";
 
 export function PostsList() {
-  const { data, isLoading } = useGetPostsQuery('')
+  const { data, isLoading } = useGetPostsQuery('');
+
   return (
     <Box py="xl" sx={{ '& > article + article': { marginTop: 24 } }}>
-      {isLoading && <Skeleton />}
-      {data && data.results.map((post) => <Post key={post.id} post={post} />)}
+      {isLoading && <Loader display="block" my="xl" mx="auto" />}
+      {data?.results.map((post) => <Post key={post.id} post={post} />)}
+      {data?.results.length === 0 &&
+        <Box my="xl">
+          <Text ta="center" color="customGray.3">No posts yet. Why not be the first to share something today?</Text>
+        </Box>
+      }
     </Box>
   )
-}
+} 
