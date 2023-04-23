144b142883d788d988b5afecd6b51184cd759155
lucasbnunes <lucasbnunes23@gmail.com>
2023-04-23 15:13:18 -0300

    fix: check if username exists before rendering the feed

--- pages/feed.tsx
+++ pages/feed.tsx
@@ -1,8 +1,15 @@
 import { Box, Container, Title } from '@mantine/core'
 import { NewPostForm } from '@/components/NewPostForm';
 import { PostsList } from '@/components/PostsList';
+import { useAppSelector } from '@/hooks/useAppSelector';
 
 export default function Feed() {
+  const { username } = useAppSelector((state) => state.user)
+
+  if (!username) {
+    return null
+  }
+
   return (
     <Container maw={800} mih='100vh' bg='white' px={0}>
       <Box bg='brand' py={27} px={37}>
